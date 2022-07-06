55980f6d2e6b2860fa90f01af603035c90a61396
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-07-06 02:24:12 -0500

    comit2

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import{ PokemonView } from "./views";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */} 
+    <PokemonView/>
   </React.StrictMode>
 )
